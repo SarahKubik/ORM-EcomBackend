d0fc7632d5f75c6b00ee2b4905350908b2efdc7b
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-12 23:27:33 -0400

    update server.js notes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Dependencies
 const express = require('express');
-// Routes
+// Import routes to sync table with database.
 const routes = require('./routes');
 // Import sequelize connection
 const sequelize =require('./config/connection');
 
-// Basic express server and port setup
+// Basic express server and port setup.
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // Sync method connects database/models and returns a promise the ".then promise."models to the database, then turn on the server to work with the data/run queries.
-
+// Note to self setting force to true will drop/create tables on every sync. Don't do in production only in dev or will lose the data after every sync.
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
